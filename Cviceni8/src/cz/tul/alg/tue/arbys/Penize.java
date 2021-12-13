0701e00b651ece34937b8eca984db43c1e0e282f
arbyys <adam.petricek@centrum.cz>
2021-12-13 14:02:02 +0100

    cv8

---
+++ Cviceni8/src/cz/tul/alg/tue/arbys/Penize.java
@@ -0,0 +1,10 @@
+package cz.tul.alg.tue.arbys;
+
+import java.util.Scanner;
+
+public class Penize {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
