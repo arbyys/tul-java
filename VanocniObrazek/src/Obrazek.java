8f1f69a9c33b45eb159e6c6e2fb8f8add8859fd0
arbyys <adam.petricek@centrum.cz>
2021-12-13 12:57:09 +0100

    gaga

--- VanocniObrazek/src/Obrazek.java
+++ VanocniObrazek/src/Obrazek.java
@@ -1,10 +1,13 @@
-package cz.tul.alg.tue.arbys;
-
 import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Pozdrav {
+public class Obrazek {
+    public static String repeatString(int count, String s)
+    {
+        return String.join("", Collections.nCopies(count, s));
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
@@ -14,7 +17,7 @@ public class Pozdrav {
         final int ONE_BRICK_LENGTH = 4;
         String output = "";
 
-        System.out.println("Zadej počet cihel krbu - sudé číslo, min. 8: ");
+        System.out.println("Zadej počet cihel velmi horkého krbu - sudé číslo, min. 8: ");
         int bricks = sc.nextInt();
         bricks = Math.max(bricks, 8);
         if(bricks%2!=0)
@@ -22,7 +25,7 @@ public class Pozdrav {
             bricks++;
         }
 
-        System.out.println("Zadej frekvenci kouře - 1 až 5: ");
+        System.out.println("Zadej frekvenci kouře velmi horkého krbu - 1 až 5: ");
         int frequency = sc.nextInt();
         frequency = (frequency < 1) || (frequency > 5) ? 3 : frequency;
 
@@ -41,18 +44,18 @@ public class Pozdrav {
                 }
                 smokeString+=" ";
             }
-            output+= String.format("%s%s\n", (String.join("", Collections.nCopies((bricksBeforeChimney*ONE_BRICK_LENGTH)-2, " "))), smokeString);
+            output+= String.format("%s%s\n", repeatString((bricksBeforeChimney*ONE_BRICK_LENGTH)-2, " "), smokeString);
         }
 
-        output += String.format("%s.%s.\n", (String.join("", Collections.nCopies(bricksBeforeChimney*ONE_BRICK_LENGTH, " "))), (String.join("", Collections.nCopies(ONE_BRICK_LENGTH, "_"))));
+        output += String.format("%s.%s.\n", repeatString(bricksBeforeChimney*ONE_BRICK_LENGTH, " "), repeatString(ONE_BRICK_LENGTH, "_"));
         for (int i = 0; i < 5; i++) {
             String currentVariable = (i%2==0 ? evenSegment : oddSegment);
             if(i == 4)
             {
-                output += String.format("%s|%s|%s\n", (String.join("", Collections.nCopies(bricksBeforeChimney*ONE_BRICK_LENGTH, "_"))), currentVariable, (String.join("", Collections.nCopies((((bricks-bricksBeforeChimney-2)*ONE_BRICK_LENGTH)+ONE_BRICK_LENGTH), "_"))));
+                output += String.format("%s|%s|%s\n", repeatString(bricksBeforeChimney*ONE_BRICK_LENGTH, "_"), currentVariable, repeatString(((bricks-bricksBeforeChimney-2)*ONE_BRICK_LENGTH)+ONE_BRICK_LENGTH, "_"));
                 continue;
             }
-            output += String.format("%s|%s|\n", (String.join("", Collections.nCopies(bricksBeforeChimney*ONE_BRICK_LENGTH, " "))), currentVariable);
+            output += String.format("%s|%s|\n", repeatString(bricksBeforeChimney*ONE_BRICK_LENGTH, " "), currentVariable);
 
         }
 
@@ -61,11 +64,11 @@ public class Pozdrav {
             String currentVariable = (i%2==0 ? evenSegment : oddSegment);
             if(i >= 5)
             {
-                String fireRow = i == 5 ? (String.join("", Collections.nCopies(spacesMiddle, " "))) : (i == 6 ? String.format(" %s ", (String.join("", Collections.nCopies((spacesMiddle-2)/2, "1 ")))) : String.format(" %s ", (String.join("", Collections.nCopies(spacesMiddle-2, "=")))));
-                output += String.format("|%s|%s|%s|\n", (String.join("", Collections.nCopies(bricksInSlopes, currentVariable))), fireRow, (String.join("", Collections.nCopies(bricksInSlopes, currentVariable))));
+                String fireRow = i == 5 ? (repeatString(spacesMiddle, " ")) : ((i == 6) ? (String.format(" %s ", repeatString((spacesMiddle-2)/2, "i "))) : (String.format(" %s ", repeatString(spacesMiddle-2, "="))));
+                output += String.format("|%s|%s|%s|\n", repeatString(bricksInSlopes, currentVariable), fireRow, repeatString(bricksInSlopes, currentVariable));
                 continue;
             }
-            output += String.format("|%s|\n", (String.join("", Collections.nCopies(bricks, currentVariable))));
+            output += String.format("|%s|\n", repeatString(bricks, currentVariable));
         }
 
         System.out.println(output);
